e1a76e7089cd4cd3f4238999963687d2ca7682ea
stira <ralf.stich@infoniqa.com>
2024-05-28 08:38:33 +0200

    chore: Update booking type controller routes
    The routes in the booking type controller have been updated to use more descriptive names and follow the RESTful convention.

--- src/controllers/bookings.controller.ts
+++ src/controllers/bookings.controller.ts
@@ -1,10 +1,9 @@
-import { Controller, Get, Query, Sse } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { interval, map, takeWhile, type Observable } from 'rxjs';
 import { BookingService } from '../services/bookings.service';
 
 @ApiTags('Bookings')
-@Controller('Bookings')
+@Controller('bookings')
 export class BookingsController {
   constructor(private readonly bookingsService: BookingService) {}
 
@@ -13,19 +12,8 @@ export class BookingsController {
     return await this.bookingsService.findAll();
   }
 
-  @Get('getForId/:id')
+  @Get('getForId')
   async getForId(@Query('id') id: number) {
     return await this.bookingsService.getForId(id);
-  }
-
-  @Sse('sse')
-  sse(): Observable<MessageEvent> {
-    return interval(333).pipe(
-      map(
-        (value) =>
-          ({ data: { hello: `world: ${value}`, value } }) as MessageEvent,
-      ),
-      takeWhile((ApiTags) => ApiTags.data.value < 100),
-    );
   }
 }
